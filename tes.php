17d523f89cad0a3111d53e7c9e6e930ec21a54b1
armand342 <54004163+armand342@users.noreply.github.com>
2021-05-29 22:49:25 +0900

    Create tes.php

--- tes.php
+++ tes.php
@@ -262,9 +262,8 @@ echo "$putih Prediksi Ekor :$hijau [$biru$b".$hijau."] |";
 	echo "$putih Prediksi Angka :$hijau [$biru$rand".$hijau."] \n";
 echo $lin;
 }
-$b = 120;
-for ($b >= 0; $b--;) {
-echo "$kuning Menunggu hasil lotrey :$putih $b  \r";
+for ($b >= $c; $b--;) {
+echo "$kuning Menunggu hasil lotrey :$putih $a:$b  \r";
 sleep (1);
 }
 }
